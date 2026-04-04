e864109e573ffada33e5879ef1ad6373b499d72f
Maciek Mazurek <mazurek.maciek.mail@gmail.com>
2026-04-05 01:24:10 +0200

    adjusted cli runner - simplified

--- src/cli/run.py
+++ src/cli/run.py
@@ -3,45 +3,27 @@ import click
 import numpy as np
 import pandas as pd
 
+from config import ROOT
 from src.opt.engine import Engine
 from src.postprocess.opt_results import OptResults
-from src.preprocess.config import Configuration
-from src.preprocess.load import load_input_data
-from src.preprocess.model import InputData
-
-def extract_energy_prices(
-    input_data: InputData,
-    start: pd.Timestamp | None = None,
-    end: pd.Timestamp | None = None
-) -> np.ndarray:
-    start = start or input_data.params.timestep_start
-    end = end or input_data.params.timestep_end
-    dt = input_data.market_data.resolution
-    date_range = pd.date_range(start=pd.to_datetime(start), end=pd.to_datetime(end), freq=f"{int(60 * dt)}min")[:-1]
-    return input_data.market_data.prices[date_range].values
+from src.preprocess.load import load_storage_static_params
+from src.preprocess.model import StorageStaticParams
 
 
-def run_opt(input_data: InputData) -> pd.DataFrame:
-    engine = Engine(input_data)
+def run_opt(storage_static_params: StorageStaticParams, dt: float) -> pd.DataFrame:
+    engine = Engine(storage_static_params, dt)
     engine.build()
-    engine.optimize(
-        lambda_penalty=np.ones(engine.t_idx.t_idx.vals.shape),
-        energy_price=extract_energy_prices(input_data),
-        init_soc=input_data.storage_state_params.soc,
-        cap=input_data.storage_static_params.technical.capacity,
+    engine.update_dynamic_parameters(
+        lambda_penalty=np.ones(len(engine.t_idx)),
+        energy_price=np.random.random(size=len(engine.t_idx)),
+        init_soc=storage_static_params.technical.capacity * storage_static_params.technical.soc_limits.min,
+        cap=storage_static_params.technical.capacity,
     )
+    engine.optimize(debug=True)
     results = OptResults.from_engine(engine)
     return results.to_dataframe()
 
 
-@click.command()
-@click.option(
-    '--config',
-    '-c',
-    required=True,
-    type=click.Path(exists=True),
-)
-def cli_run(config: Path) -> None:
-    configuration = Configuration.load(Path(config))
-    input_data = load_input_data(configuration)
-    results_df = run_opt(input_data)
+def cli_run() -> None:
+    storage_static_params = load_storage_static_params(Path(ROOT / 'assets' / 'input' / 'storage_static_params.yaml'))
+    run_opt(storage_static_params, dt=0.5)
